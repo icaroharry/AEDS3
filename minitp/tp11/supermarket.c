ffc928600e8b7b4fc54ea5a70b28a122ce872d30
Ícaro Harry <icaropc17@gmail.com>
2015-06-19 17:05:08 -0300

    PA11

--- minitp/tp11/supermarket.c
+++ minitp/tp11/supermarket.c
@@ -1,27 +1,66 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX 101
+#define INFINITY 200000
+
+float M[100002][102];
+int list[102];
+int product[100002];
+float product_price[100002];
+
+float mr_jones_sucks(int n, int m) {
+    int i, j, k, l;
+    for(i = 1; i <= n; i++) {
+        for(j = 1; j <= m; j++) {
+            if(product[i-1] == list[j-1]) {
+                if(M[i-1][j-1] + product_price[i-1] < M[i-1][j]) {
+                    M[i][j] = M[i-1][j-1] + product_price[i-1];
+                } else {
+                    M[i][j] = M[i-1][j];    
+                }
+            } else {
+                M[i][j] = M[i-1][j];
+            }
+        }
+    }
+    return M[n][m];
+}
 
 int main() {
     int m, n, i, j, k;
-    double p;
-    int products[MAX];
+    float p, result;
     char c;
 
     while(1) {
         
         scanf("%d %d\n", &m, &n);
-        if(m == 0 && m == 0) {
+        if(m == 0 && n == 0) {
             break;
         }
 
-        for(i = 0; i < m; i++)
-            scanf("%d ", &products[i]);
+        for(i = 0; i < m; i++) {
+            scanf("%d ", &list[i]);
+        }
 
-        for(j = 0; j < m; j++)
+        getchar();
+
+        for(j = 0; j < n; j++) {
             scanf("%d %f\n", &k, &p);
-    
+            product[j] = k;
+            product_price[j] = p;
+        }
+
+        for(i = 0; i <= m; i++) {
+            M[i][0] = 0;
+            M[0][i] = INFINITY;
+        }
+
+        result = mr_jones_sucks(n, m);
+        if(result == INFINITY) {
+            printf("Impossible\n");
+        } else {
+            printf("%.2f\n", result);
+        }    
     }
     
     
