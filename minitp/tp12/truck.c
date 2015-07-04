a3e66731338ff44027432e324a08fa6e658e2048
Ícaro Harry <icaropc17@gmail.com>
2015-07-04 19:31:53 -0300

    LAST FUCKING TPPPP

--- minitp/tp12/truck.c
+++ minitp/tp12/truck.c
@@ -1,38 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
-    int n, weight[101], painels, trucks, freight i, j, k;
-
-    scanf("%d\n", &n);
-
-    for(i = 0; i < n; i++) {
-        scanf("%d %d %d", &painels, &trucks, &freight);
-        for(j = 0; j < painels; j++) {
-            /* code */
+int fill_trucks(int truck[11], int weight[101], int t, int n) {
+    int i, j, max = 0, k = 0;
+    int aux[11] = { 0 };
+    truck[0] += weight[0];
+    for(i = 1; i < n; i++) {
+        //printf("%d\n", weight[i]);
+        if(truck[k] + aux[k] <= weight[i] + (truck[k+1] - aux[k])) {
+            truck[k+1] += weight[i];
+            if(k < t) {
+                k++;
+            }
+        } else {
+            truck[k] += weight[i];
         }
     }
 
-    getchar();
-
-    for(j = 0; j < n; j++) {
-        scanf("%d %f\n", &k, &p);
-        product[j] = k;
-        product_price[j] = p;
+    for(j = 0; j < t; j++) {
+        printf("%d\n", truck[j]);
+        if(truck[j] > max) {
+            max = truck[j];
+        }
     }
+    return max;
+}
 
-    for(i = 0; i <= m; i++) {
-        M[i][0] = 0;
-        M[0][i] = INFINITY;
+int main() {
+    int n, weight[101], painels, trucks, freight, i, j, k;
+    int t[11] = { 0 };
+    scanf("%d\n", &n);
+
+    for(i = 0; i < n; i++) {
+        scanf("%d %d %d\n", &painels, &trucks, &freight);
+        for(j = 0; j < painels; j++) {
+            scanf("%d ", &weight[j]);
+        }
+        printf("ss%d\n", fill_trucks(t, weight, trucks, painels));
     }
-
-    result = mr_jones_sucks(n, m);
-    if(result == INFINITY) {
-        printf("Impossible\n");
-    } else {
-        printf("%.2f\n", result);
-    }
-
-
     return 0;
 }
